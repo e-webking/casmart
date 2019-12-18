0d97bddd43516622eebf4ab3d7c27ce91d20453f
Anisur <anisur.mullick@gmail.com>
2019-12-18 13:24:50 +0530

    [C] Templates till Dec 18

--- typo3conf/ext/armtpl/Resources/Public/js/custom.js
+++ typo3conf/ext/armtpl/Resources/Public/js/custom.js
@@ -1,3 +1,35 @@
+jQuery.fn.centerElement = function () {
+   this.css ("position", "absolute");
+   this.css ("top", ($(window).height() - this.height()) / 2 + $(window).scrollTop() + "px");
+   this.css ("left", ($(window).width() - this.width()) / 2 + $(window).scrollLeft() + "px")
+   
+   return this;
+}
+
+function getPosition(el) {
+  var xPosition = 0;
+  var yPosition = 0;
+ 
+  while (el) {
+    if (el.tagName == "BODY") {
+      // deal with browser quirks with body/window/document and page scroll
+      var xScrollPos = el.scrollLeft || document.documentElement.scrollLeft;
+      var yScrollPos = el.scrollTop || document.documentElement.scrollTop;
+ 
+      xPosition += (el.offsetLeft - xScrollPos + el.clientLeft);
+      yPosition += (el.offsetTop - yScrollPos + el.clientTop);
+    } else {
+      xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
+    }
+ 
+    el = el.offsetParent;
+  }
+  return {
+    x: xPosition,
+    y: yPosition
+  };
+}
 
 $(".swiper-container").each(function(index, element){
     
@@ -61,5 +93,41 @@ document.addEventListener(
 //        $(this).siblings('.armoverlay').hide();
 //        $(this).siblings('.cta').hide();
 //    });
+    var hovertitle;
+    var hoversrc;
+    $('.icon-mouse-event').on('mouseenter', function (){
+     
+        hovertitle = $(this).find('img').attr('src');
+        hoversrc = $(this).find('img').attr('data-title');
+        $(this).find('img').attr('src', hoversrc);
+        $(this).find('img').attr('data-title', hovertitle);
+        
+    });
+    $('.icon-mouse-event').on('mouseleave', function (){
+        hoversrc = $(this).find('img').attr('data-title');
+        hovertitle = $(this).find('img').attr('src');
+        $(this).find('img').attr('src', hoversrc);
+        $(this).find('img').attr('data-title', hovertitle);
+    });
+    var nicon = '/typo3conf/ext/armaimeos/Resources/Public/Icons/info-button-black.png';
+    var hicon = '/typo3conf/ext/armaimeos/Resources/Public/Icons/info-button-white.png';
+    var mouseX, mouseY;
     
+    $(document).mousemove(function(e) {
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+    }); 
+    
+   
+    $('.arminfo').on('click', function(){
+       $(this).css("background-image", "url(" + hicon + ")");
+       $(this).siblings('.arm-aimeos-info').show();
+       
+    });
+    $('.arm-aimeos-info').on('click', function (){
+       $(this).hide(); 
+       $('.arminfo').css("background-image", "url(" + nicon + ")");
+    });
 })(jQuery);
+
+
